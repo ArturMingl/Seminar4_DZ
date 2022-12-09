b5026d5b04ef1d45f8aed046f5d0f5f08c7275a2
ArturMingl <adiscov90@gmail.com>
2022-12-09 17:04:28 +0300

    задача 29 чёт пока нет

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
+
+int[]array = new int [3];
+
+for(int index = 0; index < array.Length; index++)
+{
+    array[index] = Convert.ToInt32(Console.ReadLine()); 
+}
+
+Console.Write(array);
