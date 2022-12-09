930fea34db696edcf12baa0a9a0e5b73d32f2195
ArturMingl <adiscov90@gmail.com>
2022-12-09 17:14:09 +0300

    DZ4 Done

--- Zadacha29/Program.cs
+++ Zadacha29/Program.cs
@@ -1,10 +1,16 @@
 ﻿//Напишите программу, которая задаёт массив из 8 элементов с клавиатуры и выводит массив на экран.
 
-int[]array = new int [3];
-
-for(int index = 0; index < array.Length; index++)
+void PrintArray()
 {
-    array[index] = Convert.ToInt32(Console.ReadLine()); 
+    int[] array = new int[8];
+
+    for (int index = 0; index < array.Length; index++)
+    {
+        array[index] = Convert.ToInt32(Console.ReadLine());
+    }
+
+    Console.WriteLine(string.Join(",", array));
 }
 
-Console.Write(array);
+Console.WriteLine("Введите 8 чисел");
+PrintArray();
